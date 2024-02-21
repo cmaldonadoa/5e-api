e0f4e67c45b779c9c7d7778881c4a357eca586e3
Cristobal <cristobal.maldonado.a@gmail.com>
2024-02-21 00:23:38 -0300

    Configure JSON to GraphQL parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { parse } from "./utils/json2graphql.js";
+import * as fs from "fs";
 
-const typeDefs = ``;
+const data = fs.readFileSync(
+  process.cwd() + "/data/modified/books/books.json",
+  "utf-8",
+);
 
-const resolvers = {};
+const books = JSON.parse(data);
+const typeDefs = `type Query {\n_: Boolean\n}\n\n` + parse(books, "books");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 5555 },
