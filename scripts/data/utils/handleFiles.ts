ebe1616a2453f3e1176fab1c12823248cc93d669
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-01 19:56:34 -0300

    Add types and fix missing use cases

---
+++ scripts/data/utils/handleFiles.ts
@@ -0,0 +1,181 @@
+import fs from "fs";
+import path from "path";
+import { utils } from "./utils";
+
+type OptionsTestsEnabled = {
+  enabled: true;
+  property: string;
+  key: string;
+};
+
+type OptionsTestsDisabled = {
+  enabled: false;
+  property?: string;
+  key?: string;
+};
+
+type OptionsCustomTestsEnabled = {
+  enabled: true;
+  tests: Function;
+  property: string;
+  key: string;
+};
+
+type OptionsCustomTestsDisabled = {
+  enabled: false;
+  tests?: Function;
+  key?: string;
+  property?: string;
+};
+
+type OptionsPeekEnabled = {
+  enabled: true;
+  peek: Function;
+  key: string;
+};
+
+type OptionsPeekDisabled = {
+  enabled: false;
+  peek?: Function;
+  key?: string;
+};
+
+type Options = {
+  input: string;
+  output: string;
+  enableInternalTests?: OptionsTestsEnabled | OptionsTestsDisabled;
+  enableCustomTests?: OptionsCustomTestsEnabled | OptionsCustomTestsDisabled;
+  peek?: OptionsPeekEnabled | OptionsPeekDisabled;
+};
+
+export const handleFiles = (handlers: any, options: Options) => {
+  const files = fs.readdirSync(options.input);
+  const jsonFiles = files.filter(
+    (item) =>
+      /.json$/.test(item) && !/^foundry/.test(item) && !/^optional/.test(item),
+  );
+
+  const keys = {};
+
+  const add = (key: string) => {
+    if (key in keys) keys[key] += 1;
+    else keys[key] = 1;
+    return true;
+  };
+
+  const displayProperties = (element: any) =>
+    Object.keys(element).forEach((k) => add(k));
+
+  const runInternalTests = (element: any) => {
+    add("_total");
+    const property = options.enableInternalTests?.property;
+
+    if (element[property]) {
+      let value = element[property];
+
+      // Count occurrences
+      add("_subtotal");
+
+      // Test property types
+      add("PROP_TYPE_" + ((Array.isArray(value) && "array") || typeof value));
+
+      if (typeof value === "string")
+        value.length < 6 && add("STRING_VALUE_" + value);
+
+      if (!Array.isArray(value))
+        // Test object keys
+        Object.keys(value).forEach((k) => add(k));
+
+      if (Array.isArray(value)) {
+        // Test property array elements types
+        value.forEach((v) =>
+          typeof v !== "string"
+            ? add("ARRAY_OBJECT") &&
+              Object.keys(v).forEach((k) =>
+                add(
+                  `ARRAY_OBJECT_KEY_${k}_` +
+                    ((Array.isArray(v[k]) && "array") || typeof v[k]),
+                ),
+              )
+            : add("ARRAY_STRING"),
+        );
+
+        // Test property array length
+        if (value.length) add("ARRAY_LENGTH_" + value.length);
+      }
+    }
+  };
+
+  const peek = (element: any, key: string) => {
+    if (options.peek?.enabled && options.peek?.key === key)
+      options.peek?.peek(element);
+    return element;
+  };
+
+  const runCustomTests = (element: any) => {
+    const property = options.enableCustomTests?.property;
+
+    if (element[property]) {
+      options.enableCustomTests?.tests(element);
+    }
+  };
+
+  for (let filename of jsonFiles) {
+    const data = fs.readFileSync(options.input + "/" + filename, "utf8");
+    const oldData = JSON.parse(data);
+    const additional = {};
+    const newData = Object.keys(handlers).reduce((accumulator, key) => {
+      accumulator[key] = oldData[key].map((element: any) => {
+        const handle = (x: any) => {
+          const copy = utils.getCopy(oldData[key], x._copy);
+
+          if (
+            options.enableInternalTests?.enabled &&
+            options.enableInternalTests?.key === key
+          )
+            runInternalTests(copy || x);
+
+          if (
+            options.enableCustomTests?.enabled &&
+            options.enableCustomTests?.key === key
+          )
+            runCustomTests(copy || x);
+
+          if (
+            !options.enableInternalTests?.enabled &&
+            !options.enableCustomTests?.enabled &&
+            !options.peek?.enabled
+          )
+            displayProperties(copy || x);
+
+          const result = handlers[key](copy || x);
+          return peek(result, key);
+        };
+
+        const versions = utils.getVersions(element);
+        if (versions.length > 1) {
+          additional[key] = [
+            ...(additional[key] || []),
+            ...versions.map((version) => handle(version)),
+          ];
+        } else return handle(element);
+      });
+      return accumulator;
+    }, {});
+
+    Object.keys(newData).forEach(
+      (k) => (newData[k] = newData[k].filter(Boolean)),
+    );
+
+    Object.keys(additional).forEach(
+      (k) => (newData[k] = [...newData[k], ...additional[k]]),
+    );
+
+    const output = `${options.output}/${path.basename(options.input)}/`;
+
+    fs.mkdirSync(output, { recursive: true });
+    fs.writeFileSync(output + filename, JSON.stringify(newData));
+  }
+
+  console.log(keys);
+};
---
+++ scripts/data/utils/utils.ts
@@ -0,0 +1,423 @@
+interface BaseData {
+  name?: string;
+  source?: string;
+  entries?: Entries;
+}
+
+interface FieldData {
+  choose?: string | ChooseData;
+  count?: number;
+}
+
+type ChooseData = {
+  fromFilter?: string;
+  from?: string | string[];
+  count?: number;
+};
+
+type SpellData = {
+  [K: string | number]:
+    | string[]
+    | { will: string[] }
+    | { ritual: string[] }
+    | { daily: { [k: string | number]: string[] } }
+    | { rest: { [k: string | number]: string[] } }
+    | { resource: { [k: string | number]: string[] } };
+};
+
+type SpellItem = {
+  _meta?: {
+    level?: number;
+    will?: true;
+    ritual?: true;
+    longRest?: 1;
+    shortRest?: 1;
+  };
+  item: any;
+};
+
+interface Copy {
+  name: string;
+  source: string;
+  _mod?: { entries: ModifierEntries };
+}
+
+interface Copyable extends BaseData {
+  _copy?: Copy;
+}
+
+interface Versioned extends BaseData {
+  _versions?: (Copy | TemplateVersion)[];
+}
+
+interface TemplateVersion {
+  _template?: Copy;
+  _implementations?: { _variables: string[] }[];
+}
+
+interface ModifierEntry {
+  names?: string;
+  replace?: string | { index?: number };
+  mode?: string;
+  index?: number;
+  items?: TypedEntry;
+}
+
+interface ItemEntry {
+  name: string;
+  entriesTemplate: Entries;
+}
+
+type TypedEntry = {
+  type: string;
+  name?: string;
+  items?: Entries;
+  entries?: Entries;
+};
+
+type Entry = string | TypedEntry;
+
+type Entries = Entry | Entry[];
+
+type ModifierEntries = ModifierEntry | ModifierEntry[];
+
+const modifyEntries = (base: BaseData, modifierEntries: ModifierEntry[]) => {
+  modifierEntries.forEach((entry) => {
+    if (typeof entry === "string") return;
+
+    if (entry.mode === "removeArr")
+      (base.entries as Entry[]) = (base.entries as Entry[]).filter(
+        (e) => typeof e !== "string" && e.name !== entry.names,
+      );
+    else if (entry.mode === "replaceArr")
+      (base.entries as Entry[])[
+        typeof entry.replace === "string"
+          ? (base.entries as Entry[]).findIndex(
+              (e) => typeof e !== "string" && e.name === entry.replace,
+            )
+          : entry.replace.index
+      ] = entry.items;
+    else if (entry.mode === "insertArr")
+      (base.entries as Entry[]).splice(entry.index, 0, entry.items);
+  });
+  return base;
+};
+
+export const utils = {
+  asArray: <T>(object: T | T[]): T[] =>
+    object ? (Array.isArray(object) ? object : [object]) : [],
+
+  adapt: <T>(object: T): undefined | T => {
+    if (!object || (Array.isArray(object) && object.length === 0)) return;
+    return object;
+  },
+
+  formatEntries: (entries: Entry[]): TypedEntry[] => {
+    return entries.map((entry) =>
+      typeof entry === "string"
+        ? { type: "text", entry }
+        : entry.type === "list"
+          ? {
+              ...entry,
+              items: utils.adapt(
+                utils.formatEntries(utils.asArray(entry.items)),
+              ),
+            }
+          : {
+              ...entry,
+              entries: utils.adapt(
+                utils.formatEntries(utils.asArray(entry.entries)),
+              ),
+            },
+    );
+  },
+
+  renderItemEntries: (item: BaseData, itemEntries: ItemEntry[]) => {
+    if (
+      !item.entries ||
+      !Array.isArray(item.entries) ||
+      typeof item.entries[0] !== "string" ||
+      item.entries[0].search("#itemEntry") === -1
+    )
+      return undefined;
+
+    const [name] = item.entries[0].match(/{#itemEntry (.*)}/)[1].split("|");
+
+    function replaceVariables(entries: Entries): Entries {
+      const replace = (entry: Entry): Entry => {
+        if (typeof entry !== "string")
+          return {
+            ...entry,
+            entries: replaceVariables(entry.entries),
+          };
+
+        const regexp = /{{item\.(\w*)}}/g;
+        const matches = entry.match(regexp);
+
+        if (!matches) return entry;
+
+        const variables = matches.map((m) => m.match(regexp)[1]);
+        return variables.reduce((accumulator, variable) => {
+          const variableRegexp = new RegExp(`{{item\\.${variable}}}`, "g");
+          return accumulator.replace(variableRegexp, item[variable]);
+        }, entry);
+      };
+
+      if (!Array.isArray(entries)) return replace(entries);
+      return entries.map(replace);
+    }
+
+    const itemEntry = itemEntries.find((e) => e.name === name);
+    return replaceVariables(itemEntry.entriesTemplate);
+  },
+
+  splitEntries: (entries?: Entries) => {
+    const split = (nextId: number, entries?: Entries) => {
+      if (!entries) return;
+
+      let result = [];
+      utils.formatEntries(utils.asArray(entries)).forEach((entry) => {
+        const descendantNodes = split(nextId + 1, entry.entries) || [];
+        const children = utils.adapt(
+          descendantNodes.filter((e) => e.parentId === nextId).map((e) => e.id),
+        );
+        delete entry.entries;
+        result = [
+          ...result,
+          {
+            ...entry,
+            id: nextId,
+            children,
+            parentId: nextId ? nextId - 1 : undefined,
+          },
+          ...descendantNodes,
+        ];
+        nextId += 1 + descendantNodes.length;
+      });
+      return result;
+    };
+    return split(0, entries);
+  },
+
+  getCopy: (data: Copyable[], copy?: Copy) => {
+    if (!copy) return;
+
+    let base = {
+      ...data.find((e) => e.name === copy.name && e.source === copy.source),
+    };
+
+    if (base._copy) base = utils.getCopy(data, base._copy);
+
+    delete base.name;
+    delete base.source;
+
+    if (copy._mod) {
+      const entries = utils.asArray(copy._mod.entries);
+      base = modifyEntries(base, entries);
+    }
+
+    return base;
+  },
+
+  formatAdditionalSpells: (keys: string[], spells?: SpellData) => {
+    if (!spells) return [];
+
+    //const keys = [...Array(20).keys()].map(k => "" + (k + 1));
+    const spellList = [];
+
+    const dailyKeys = ["1", "1e"];
+    const restKeys = ["1"];
+    const resourceKeys = ["2", "3", "4", "5", "6"];
+
+    keys.forEach((level) => {
+      const spell = spells[level];
+
+      if (!spell) return;
+
+      let spellItems: SpellItem[];
+
+      if (Array.isArray(spell))
+        spellItems = spell.map((o) => ({
+          item: utils.formatObject(o),
+          ...(Number.isInteger(level) && {
+            _meta: {
+              level: +level,
+            },
+          }),
+        }));
+      else if ("will" in spell)
+        spellItems = spell.will.map((o) => ({
+          item: utils.formatObject(o),
+          _meta: {
+            ...(Number.isInteger(level) && { level: +level }),
+            will: true,
+          },
+        }));
+      else if ("ritual" in spell)
+        spellItems = spell.ritual.map((o) => ({
+          item: utils.formatObject(o),
+          _meta: {
+            ...(Number.isInteger(level) && { level: +level }),
+            ritual: true,
+          },
+        }));
+      else if ("daily" in spell)
+        spellItems = dailyKeys.reduce((accumulator, key) => {
+          const newSpells = spell.daily[key].map((o) => ({
+            item: utils.formatObject(o),
+            _meta: {
+              ...(Number.isInteger(level) && { level: +level }),
+              longRest: 1,
+            },
+          }));
+          return [...accumulator, ...newSpells];
+        }, []);
+      else if ("rest" in spell)
+        spellItems = restKeys.reduce((accumulator, key) => {
+          const newSpells = spell.rest[key].map((o) => ({
+            item: utils.formatObject(o),
+            _meta: {
+              ...(Number.isInteger(level) && { level: +level }),
+              shortRest: 1,
+            },
+          }));
+          return [...accumulator, ...newSpells];
+        }, []);
+      else if ("resource" in spell)
+        spellItems = resourceKeys.reduce((accumulator, key) => {
+          const newSpells = spell.resource[key].map((o) => ({
+            item: utils.formatObject(o),
+            ...(Number.isInteger(level) && {
+              _meta: {
+                level: +level,
+              },
+            }),
+          }));
+          return [...accumulator, ...newSpells];
+        }, []);
+
+      spellList.push(...spellItems);
+    });
+
+    return spellList;
+  },
+
+  formatObject: (object?: FieldData | string | (FieldData | string)[]) => {
+    if (!object) return;
+
+    if (Array.isArray(object)) return object.map((e) => utils.formatObject(e));
+
+    if (typeof object === "string") return { item: utils.clearName(object) };
+
+    const getItems = () => Object.keys(object).filter((e) => e !== "choose");
+    const hasItems = () => getItems().length > 0;
+
+    if ("choose" in object) {
+      if (typeof object.choose === "string")
+        return {
+          ...(hasItems() && { items: getItems() }),
+          choose: {
+            fromFilter: object.choose,
+            count: object.count || 1,
+          },
+        };
+      else if ("from" in object.choose)
+        return {
+          ...(hasItems() && { items: getItems() }),
+          choose: {
+            ...(Array.isArray(object.choose.from) && {
+              from: object.choose.from.map((n) => utils.clearName(n)),
+            }),
+            ...(!Array.isArray(object.choose.from) && {
+              fromFilter: object.choose.from,
+            }),
+            count: object.choose.count || 1,
+          },
+        };
+      else
+        return {
+          ...(hasItems() && { items: getItems() }),
+          choose: {
+            ...(Array.isArray(object.choose) && {
+              from: object.choose.map((n) => utils.clearName(n)),
+            }),
+            ...(!Array.isArray(object.choose) && {
+              fromFilter: object.choose.fromFilter || object.choose,
+            }),
+            count: object.count || 1,
+          },
+        };
+    }
+    return hasItems()
+      ? {
+          items: getItems(),
+        }
+      : undefined;
+  },
+
+  clearName: (name: string) => {
+    return name.split("#")[0];
+  },
+
+  getVersions: (original: Versioned): BaseData[] => {
+    if (!("_versions" in original)) return [];
+
+    let versions = original._versions;
+    delete original._versions;
+
+    if (versions.length === 1) {
+      const version = versions[0] as TemplateVersion;
+      const template = version._template;
+      const implementations = version._implementations;
+
+      const replaceVariables = (text: string, vars: string[]) => {
+        const regexp = new RegExp(
+          "{{(" + Object.keys(vars).join("|") + ")}}",
+          "g",
+        );
+        return text.replace(regexp, (m, $1) => vars[$1] || m);
+      };
+
+      versions = implementations.map((implementation) => {
+        const variables = implementation._variables;
+        delete implementation._variables;
+
+        return {
+          ...template,
+          ...implementation,
+          name: replaceVariables(template.name, variables),
+          ...(template._mod && {
+            _mod: {
+              entries: utils.asArray(template._mod.entries).map((entry) => ({
+                ...entry,
+                items: entry.items
+                  ? {
+                      ...entry.items,
+                      entries: utils
+                        .asArray(entry.items.entries)
+                        .map((e) => replaceVariables(e as string, variables)),
+                    }
+                  : undefined,
+              })),
+            },
+          }),
+        };
+      });
+    }
+
+    return versions.map((version: Copy) => {
+      if (original.entries && version._mod) {
+        let entriesMod: ModifierEntry[] = utils.asArray(version._mod.entries);
+        delete version._mod;
+
+        original = modifyEntries(original, entriesMod);
+      }
+
+      return {
+        ...original,
+        ...version,
+        entries: original.entries,
+      };
+    });
+  },
+};
