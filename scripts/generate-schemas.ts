c644a26adf107e09738ac981053ef14282099fc4
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-01 20:00:18 -0300

    Remove unused import

--- scripts/generate-schemas.ts
+++ scripts/generate-schemas.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
 import rootDir from "app-root-dir";
 import { spawnSync } from "child_process";
-import { basename } from "path";
 
 const root = rootDir.get();
 const path = root + "/data/original/";
@@ -9,21 +8,21 @@ const path = root + "/data/original/";
 const folders = fs.readdirSync(path);
 
 for (const name of folders) {
-    console.log("---", name);
-    const { stdout, stderr } = spawnSync("npm", [
-        "run",
-        "generate-schema",
-        "--",
-        root + "/data/modified/" + name,
-        root + "/schemas/",
-        "--",
-        "foundry.json",
-        "optionalfeatures.json"
-    ]);
-    console.log(stdout.toString());
+  console.log("---", name);
+  const { stdout, stderr } = spawnSync("npm", [
+    "run",
+    "generate-schema",
+    "--",
+    root + "/data/modified/" + name,
+    root + "/schemas/",
+    "--",
+    "foundry.json",
+    "optionalfeatures.json",
+  ]);
+  console.log(stdout.toString());
 
-    if (stderr.toString()) {
-        console.log(stderr.toString());
-        break;
-    }
+  if (stderr.toString()) {
+    console.log(stderr.toString());
+    break;
+  }
 }
