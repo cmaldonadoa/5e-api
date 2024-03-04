86b47d6650e34dad7aa99856644e0f334bfd6ecb
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-04 19:41:43 -0300

    Fix TypeDefs and add resolvers

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,70 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { loadSchema } from "@graphql-tools/load";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import rootDir from "app-root-dir";
 import fs from "fs";
+import { Resolvers } from "./__generated__/graphql";
+import {
+  Backgrounds,
+  Books,
+  Decks,
+  Feats,
+  Items,
+  Languages,
+  Races,
+  Spells,
+  Vehicles,
+} from "./index.d";
 
-const loadedTypeDefs = await loadSchema("schemas/*.graphql", {
-    loaders: [new GraphQLFileLoader()]
+const root = rootDir.get();
+
+const loadFilesSync = <T>(dirname: string): T[] => {
+  const files = fs.readdirSync(dirname).filter((file) => /\.json$/.test(file));
+  return files.map((file) =>
+    JSON.parse(fs.readFileSync(`${dirname}/${file}`, "utf-8"))
+  );
+};
+
+const data = {
+  backgrounds: loadFilesSync<Backgrounds>(root + "/data/modified/backgrounds"),
+  books: loadFilesSync<Books>(root + "/data/modified/books"),
+  decks: loadFilesSync<Decks>(root + "/data/modified/decks"),
+  feats: loadFilesSync<Feats>(root + "/data/modified/feats"),
+  items: loadFilesSync<Items>(root + "/data/modified/items"),
+  languages: loadFilesSync<Languages>(root + "/data/modified/languages"),
+  races: loadFilesSync<Races>(root + "/data/modified/races"),
+  spells: loadFilesSync<Spells>(root + "/data/modified/spells"),
+  vehicles: loadFilesSync<Vehicles>(root + "/data/modified/vehicles"),
+};
+
+const loadedTypeDefs = await loadSchema(root + "/schemas/*.graphql", {
+  loaders: [new GraphQLFileLoader()],
 });
-const queries = `type Query {\n_: Boolean\n}\n\n`;
-const typeDefs = queries + loadedTypeDefs;
+const typeDefs = mergeTypeDefs(loadedTypeDefs);
+const resolvers: Resolvers = {
+  Query: {
+    backgrounds: () => data.backgrounds.flatMap((e) => e.background),
+    books: () => data.books.flatMap((e) => e.book),
+    decks: () => data.decks.flatMap((e) => e.deck),
+    cards: () => data.decks.flatMap((e) => e.card),
+    feats: () => data.feats.flatMap((e) => e.feat),
+    items: () => data.items.flatMap((e) => e.item),
+    baseItems: () => data.items.flatMap((e) => e.baseItem),
+    itemProperties: () => data.items.flatMap((e) => e.itemProperty),
+    itemTypes: () => data.items.flatMap((e) => e.itemType),
+    languages: () => data.languages.flatMap((e) => e.language),
+    races: () => data.races.flatMap((e) => e.race),
+    subraces: () => data.races.flatMap((e) => e.subrace),
+    spells: () => data.spells.flatMap((e) => e.spell),
+    vehicles: () => data.vehicles.flatMap((e) => e.vehicle),
+  },
+};
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 5555 }
+  listen: { port: 5555 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
