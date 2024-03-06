bb8f579df07642f4563b7857cab69af8622054db
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-06 17:49:13 -0300

    Add user register and login resolvers

---
+++ src/resolvers/authentication/index.ts
@@ -0,0 +1,58 @@
+import { Resolvers, User } from "../../__generated__/graphql";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JSONMutator } from "../utils";
+import { GraphQLError } from "graphql/error";
+import { ApolloServerErrorCode } from "@apollo/server/errors";
+
+type Users = {
+  user: User;
+};
+
+const json = new JSONMutator<Users>("/storage/authentication/users.json");
+
+export const resolvers: Resolvers = {
+  Mutation: {
+    register: async (parent, { username, password }) => {
+      const users = json.get("user", (e: User) => e.username === username);
+      if (users.length)
+        throw new GraphQLError("User is already taken", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_USER_INPUT,
+            http: { status: 403 },
+          },
+        });
+      const user: User = {
+        username,
+        password: await bcrypt.hash(password, 5),
+      };
+
+      json.add("user", user);
+      json.save();
+      return true;
+    },
+    login: async (parent, { username, password }) => {
+      const users = json.get("user", (e: User) => e.username === username);
+      if (!users.length)
+        throw new GraphQLError("Username does not exist", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_REQUEST,
+            http: { status: 400 },
+          },
+        });
+
+      const user = users[0].item;
+
+      const isValid = await bcrypt.compare(password, user.password);
+      if (!isValid)
+        throw new GraphQLError("Incorrect password", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_USER_INPUT,
+            http: { status: 403 },
+          },
+        });
+
+      return jwt.sign({ username }, process.env.JWT_KEY);
+    },
+  },
+};
