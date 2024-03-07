c4da552912ecae9a85f809c31a088a55e15c036a
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-07 01:19:52 -0300

    Use character resolvers

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import rootDir from "app-root-dir";
 import { resolvers as dataResolvers } from "./resolvers/data";
 import { resolvers as authResolvers } from "./resolvers/authentication";
+import { resolvers as charResolvers } from "./resolvers/character";
 import jwt from "jsonwebtoken";
+import { Resolvers } from "./__generated__/graphql";
 
 const root = rootDir.get();
 
@@ -15,7 +17,18 @@ const loadedTypeDefs = await loadSchema(root + "/schemas/*.graphql", {
   loaders: [new GraphQLFileLoader()],
 });
 const typeDefs = mergeTypeDefs(loadedTypeDefs);
-const resolvers = { ...dataResolvers, ...authResolvers };
+const resolvers: Resolvers = {
+  Query: {
+    ...dataResolvers.Query,
+    ...authResolvers.Query,
+    ...charResolvers.Query,
+  },
+  Mutation: {
+    ...dataResolvers.Mutation,
+    ...authResolvers.Mutation,
+    ...charResolvers.Mutation,
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
