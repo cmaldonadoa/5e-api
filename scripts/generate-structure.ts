471002da2fe477991a9c81ed512acdb3c01e2702
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-07 01:15:45 -0300

    Add try-catch block to prevent errors in app start

--- scripts/generate-structure.ts
+++ scripts/generate-structure.ts
@@ -3,20 +3,28 @@ import rootDir from "app-root-dir";
 
 const root = rootDir.get();
 
-fs.mkdirSync(root + "/storage/authentication/", { recursive: true });
-fs.writeFileSync(
-  root + "/storage/authentication/users.json",
-  JSON.stringify({
-    user: [],
-  }),
-  { flag: "wx" }
-);
+try {
+  fs.mkdirSync(root + "/storage/authentication/", { recursive: true });
+  fs.writeFileSync(
+    root + "/storage/authentication/users.json",
+    JSON.stringify({
+      user: [],
+    }),
+    { flag: "wx" }
+  );
+} catch (e) {
+  console.log("Ignoring generation of /storage/authentication/users.json");
+}
 
-fs.mkdirSync(root + "/storage/characters/", { recursive: true });
-fs.writeFileSync(
-  root + "/storage/characters/characters.json",
-  JSON.stringify({
-    character: [],
-  }),
-  { flag: "wx" }
-);
+try {
+  fs.mkdirSync(root + "/storage/characters/", { recursive: true });
+  fs.writeFileSync(
+    root + "/storage/characters/characters.json",
+    JSON.stringify({
+      character: [],
+    }),
+    { flag: "wx" }
+  );
+} catch (e) {
+  console.log("Ignoring generation of /storage/authentication/characters.json");
+}
