5e87841921297ff6de14e3f92332cc7c43195504
clolate <37159005+clolate@users.noreply.github.com>
2024-03-08 20:16:40 -0300

    Add remaining basic character mutations

--- src/resolvers/character/index.ts
+++ src/resolvers/character/index.ts
@@ -75,5 +75,149 @@ export const resolvers: Resolvers = {
         return character;
       });
     },
+    addCharacterClass: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        const classes = character.classes || [];
+        if (classes.find((e) => e.name === args.name))
+          throw new GraphQLError("Class already exists", {
+            extensions: {
+              code: ApolloServerErrorCode.BAD_REQUEST,
+              http: { status: 400 },
+            },
+          });
+        character.classes = [
+          ...classes,
+          {
+            name: args.name,
+            level: 1,
+          },
+        ];
+        return character;
+      });
+    },
+    setCharacterSubclass: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        const classes = character.classes || [];
+        const characterClass = classes.find((e) => e.name === args.className);
+        const characterClassIndex = classes.findIndex(
+          (e) => e.name === args.className
+        );
+
+        if (!characterClass)
+          throw new GraphQLError("Class does not exist", {
+            extensions: {
+              code: ApolloServerErrorCode.BAD_REQUEST,
+              http: { status: 400 },
+            },
+          });
+
+        if (characterClass.subclassName)
+          throw new GraphQLError("Subclass already set", {
+            extensions: {
+              code: ApolloServerErrorCode.BAD_REQUEST,
+              http: { status: 400 },
+            },
+          });
+        characterClass.subclassName = args.name;
+        character.classes[characterClassIndex] = characterClass;
+        return character;
+      });
+    },
+    setCharacterBackground: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.backgroundName = args.name;
+        return character;
+      });
+    },
+    setAbilityScores: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.ability = { ...character.ability, ...args.abilities };
+        return character;
+      });
+    },
+    setProficiencies: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.proficiencies = {
+          armor: [
+            ...character.proficiencies?.armor,
+            ...args.proficiencies?.armor,
+          ],
+          weapon: [
+            ...character.proficiencies?.weapon,
+            ...args.proficiencies?.weapon,
+          ],
+          language: [
+            ...character.proficiencies?.language,
+            ...args.proficiencies?.language,
+          ],
+          tool: [...character.proficiencies?.tool, ...args.proficiencies?.tool],
+          savingThrow: [
+            ...character.proficiencies?.savingThrow,
+            ...args.proficiencies?.savingThrow,
+          ],
+        };
+        return character;
+      });
+    },
+    setHitPointMaximum: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.hitPointMaximum = args.hitPoint;
+        return character;
+      });
+    },
+    setHitPointCurrent: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.hitPointCurrent = args.hitPoint;
+        return character;
+      });
+    },
+    addItem: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        return character;
+      });
+    },
+    removeItem: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        return character;
+      });
+    },
+    setCoins: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.coins = { ...character.coins, ...args.coins };
+        return character;
+      });
+    },
+    addFeature: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.features = [
+          ...character.features,
+          {
+            entries: args.entries,
+          },
+        ];
+        return character;
+      });
+    },
+    updateFeature: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.features[args.index] = {
+          entries: args.entries,
+        };
+        return character;
+      });
+    },
+    removeFeature: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        character.features[args.index] = {
+          entries: [],
+        };
+        return character;
+      });
+    },
+    levelUp: (parent, args, context) => {
+      return updateCharacter(context, args.characterName, (character) => {
+        return character;
+      });
+    },
   },
 };
