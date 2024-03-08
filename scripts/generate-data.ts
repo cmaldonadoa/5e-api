9e3edeb52dc29b939360e2b7e306f67dc3ab0474
clolate <37159005+clolate@users.noreply.github.com>
2024-03-08 19:48:10 -0300

    Migrate from ts-node to tsx

--- scripts/generate-data.ts
+++ scripts/generate-data.ts
@@ -5,20 +5,18 @@ import { spawnSync } from "child_process";
 const root = rootDir.get();
 const path = root + "/scripts/data/";
 
-const files = fs.readdirSync(path).filter(e => /\.ts$/.test(e));
+const files = fs.readdirSync(path).filter((e) => /\.ts$/.test(e));
 
 for (const filename of files) {
-    console.log("---", filename);
-    const { stdout, stderr } = spawnSync("npx", [
-        "ts-node",
-        "--esm",
-        "--experimental-specifier-resolution=node",
-        path + filename
-    ]);
-    console.log(stdout.toString());
+  console.log("---", filename);
+  const { stdout, stderr } = spawnSync("tsx", [
+    "--experimental-specifier-resolution=node",
+    path + filename,
+  ]);
+  console.log(stdout.toString());
 
-    if (stderr.toString()) {
-        console.log(stderr.toString());
-        break;
-    }
+  if (stderr.toString()) {
+    console.log(stderr.toString());
+    break;
+  }
 }
