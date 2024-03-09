8b378d0a38e3ff3968914279f1b40588dc2ec02b
Cristobal <cristobal.maldonado.a@gmail.com>
2024-03-09 20:52:41 -0300

    Refactor the mutator that sets character race

--- src/resolvers/character/index.ts
+++ src/resolvers/character/index.ts
@@ -1,8 +1,19 @@
-import { Character, Resolvers } from "../../__generated__/graphql";
+import {
+  Character,
+  CharacterAbilityScoresInput,
+  CharacterDataSource,
+  CharacterDataValue,
+  CharacterEntries,
+  CharacterFeature,
+  CharacterProficienciesInput,
+  CharacterSpellInput,
+  Resolvers,
+} from "../../__generated__/graphql";
 import { authorize, JSONMutator } from "../utils";
 import { GraphQLError } from "graphql/error";
 import { ApolloServerErrorCode } from "@apollo/server/errors";
 import { ResolverContext } from "../../context";
+import { queries as raceQueries } from "../data/races";
 
 type Characters = {
   character: Character;
@@ -27,6 +38,105 @@ const getCharacter = (owner: string, name: string) => {
   return characters[0];
 };
 
+const getFeatures = (
+  entries: any[],
+  sourceType: CharacterDataSource,
+  sourceText: string
+): CharacterFeature[] => {
+  const features = (entries as CharacterEntries[]).filter(
+    (entry) => !entry.hasOwnProperty("parentId")
+  );
+  return features.map((feature) => ({
+    entries: [
+      feature,
+      ...(entries as CharacterEntries[]).filter(
+        (entry) => entry.parentId === feature.id
+      ),
+    ],
+    sourceType: sourceType,
+    sourceText: sourceText,
+  }));
+};
+
+const addAbilityScores = (
+  character: Character,
+  abilityScores: CharacterAbilityScoresInput
+) =>
+  Object.keys(character.abilityScores).reduce((characterAbilityScores, key) => {
+    characterAbilityScores[key] += (abilityScores || {})[key] || 0;
+    return characterAbilityScores;
+  }, character.abilityScores);
+
+const addProficiencies = (
+  character: Character,
+  proficiencies: CharacterProficienciesInput,
+  handle: (e: string) => CharacterDataValue
+) =>
+  Object.keys(character.proficiencies).reduce((characterProficiencies, key) => {
+    characterProficiencies[key] = [
+      ...characterProficiencies[key],
+      ...((proficiencies || {})[key] || []).map(handle),
+    ];
+    return characterProficiencies;
+  }, character.proficiencies);
+
+const addSpells = (
+  character: Character,
+  spells: CharacterSpellInput[],
+  spellcastingAbility: string,
+  handle: (e: string) => CharacterDataValue
+) => [
+  ...character.spells,
+  ...spells.map((spell) => ({
+    spell: handle(spell.spell),
+    spellcastingAbility,
+    _meta: spell._meta,
+  })),
+];
+
+const toDataValue = (
+  value: string,
+  type: CharacterDataSource,
+  text?: string
+): CharacterDataValue => ({
+  item: value,
+  sourceType: type,
+  sourceText: text,
+});
+
+const deleteKeys = (object: any, ...keys: string[]) => {
+  keys.forEach((key) => delete object[key]);
+  return object;
+};
+
+const StringArrayAbilityScoreAdapter = (
+  abilities: string[]
+): CharacterAbilityScoresInput => {
+  const abilityScores = {
+    str: 0,
+    dex: 0,
+    con: 0,
+    int: 0,
+    wis: 0,
+    cha: 0,
+  };
+  abilities.forEach((key) => (abilityScores[key] += 1));
+  return abilityScores;
+};
+
+const ObjectProficienciesAdapter = (
+  object: any
+): CharacterProficienciesInput => {
+  return {
+    armor: object.armorProficiencies?.items || [],
+    weapon: object.weaponProficiencies?.items || [],
+    tool: object.toolProficiencies?.items || [],
+    language: object.languageProficiencies?.items || [],
+    savingThrow: object.savingThrowProficiencies?.items || [],
+    skill: object.skillProficiencies?.items || [],
+  };
+};
+
 const updateCharacter = (
   context: ResolverContext,
   characterName: string,
@@ -43,41 +153,166 @@ const updateCharacter = (
   return character;
 };
 
-export const resolvers: Resolvers = {
+export default {
   Mutation: {
-    createCharacter: (parent, args, context) => {
+    createCharacter: (_parent, args, context) => {
       authorize(context);
-      const character = {
+      const characters = json.get(
+        "character",
+        (e: Character) => e.owner === context.username && e.name === args.name
+      );
+      if (characters.length)
+        throw new GraphQLError("Character already exists", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_REQUEST,
+            http: { status: 400 },
+          },
+        });
+
+      const character: Character = {
         owner: context.username,
         name: args.name,
+        abilityScores: {
+          str: 0,
+          dex: 0,
+          con: 0,
+          int: 0,
+          wis: 0,
+          cha: 0,
+        },
+        classes: [],
+        features: [],
+        items: [],
+        baseItems: [],
+        spells: [],
+        proficiencies: {
+          armor: [],
+          weapon: [],
+          tool: [],
+          language: [],
+          savingThrow: [],
+          skill: [],
+        },
       };
       json.add("character", character);
       json.save();
       return character;
     },
-    setCharacterName: (parent, args, context) => {
+    setCharacterName: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.name = args.name;
         return character;
       });
     },
-    setCharacterRace: (parent, args, context) => {
+    setCharacterRace: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
-        character.raceName = args.name;
-        character.raceSource = args.source;
+        const race = raceQueries.race(args.raceName, args.raceSource);
+        const subrace = raceQueries.subrace(
+          args.subraceName,
+          args.subraceSource
+        );
+
+        const raceHandler = (e: string) =>
+          toDataValue(e, CharacterDataSource.Race, args.raceName);
+        const subraceHandler = (e: string) =>
+          toDataValue(
+            e,
+            CharacterDataSource.Race,
+            `${args.subraceName} ${args.raceName}`
+          );
+
+        character.raceName = args.raceName;
+        character.raceSource = args.raceSource;
+        character.subraceName = args.subraceName;
+        character.subraceSource = args.subraceSource;
+        character.features = [
+          ...character.features,
+          ...getFeatures(
+            race.entries,
+            CharacterDataSource.Race,
+            character.raceName
+          ),
+          ...getFeatures(
+            subrace.entries,
+            CharacterDataSource.Race,
+            `${character.subraceName} ${character.raceName}`
+          ),
+        ];
+        character.speed = { ...race.speed, ...subrace.speed };
+
+        // Handle race traits
+        character.abilityScores = addAbilityScores(
+          character,
+          subrace.overwrite?.ability
+            ? {}
+            : args.raceAbilityScores ||
+                StringArrayAbilityScoreAdapter(race.ability.items)
+        );
+        character.proficiencies = addProficiencies(
+          character,
+          deleteKeys(
+            ObjectProficienciesAdapter(race),
+            subrace.overwrite?.languageProficiencies ? "language" : "",
+            subrace.overwrite?.skillProficiencies ? "skill" : ""
+          ),
+          raceHandler
+        );
+        character.proficiencies = addProficiencies(
+          character,
+          args.raceProficiencies,
+          raceHandler
+        );
+        character.spells = addSpells(
+          character,
+          race.additionalSpells.spells.filter((spell) => spell.item),
+          args.raceSpellcastingAbility ||
+            race.additionalSpells?.spellcastingAbility?.items[0],
+          raceHandler
+        );
+        character.spells = addSpells(
+          character,
+          args.raceSpells,
+          args.raceSpellcastingAbility ||
+            race.additionalSpells?.spellcastingAbility?.items[0],
+          raceHandler
+        );
+
+        // Handle subrace traits
+        character.abilityScores = addAbilityScores(
+          character,
+          args.subraceAbilityScores ||
+            StringArrayAbilityScoreAdapter(subrace.ability.items)
+        );
+        character.proficiencies = addProficiencies(
+          character,
+          ObjectProficienciesAdapter(subrace),
+          subraceHandler
+        );
+        character.proficiencies = addProficiencies(
+          character,
+          args.subraceProficiencies,
+          subraceHandler
+        );
+        character.spells = addSpells(
+          character,
+          subrace.additionalSpells.spells.filter((spell) => spell.item),
+          args.subraceSpellcastingAbility ||
+            subrace.additionalSpells?.spellcastingAbility?.items[0],
+          subraceHandler
+        );
+        character.spells = addSpells(
+          character,
+          args.subraceSpells,
+          args.subraceSpellcastingAbility ||
+            subrace.additionalSpells?.spellcastingAbility?.items[0],
+          subraceHandler
+        );
         return character;
       });
     },
-    setCharacterSubrace: (parent, args, context) => {
+    addCharacterClass: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
-        character.subraceName = args.name;
-        character.subraceSource = args.source;
-        return character;
-      });
-    },
-    addCharacterClass: (parent, args, context) => {
-      return updateCharacter(context, args.characterName, (character) => {
-        const classes = character.classes || [];
+        const classes = character.classes;
         if (classes.find((e) => e.name === args.name))
           throw new GraphQLError("Class already exists", {
             extensions: {
@@ -95,9 +330,9 @@ export const resolvers: Resolvers = {
         return character;
       });
     },
-    setCharacterSubclass: (parent, args, context) => {
+    setCharacterSubclass: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
-        const classes = character.classes || [];
+        const classes = character.classes;
         const characterClass = classes.find((e) => e.name === args.className);
         const characterClassIndex = classes.findIndex(
           (e) => e.name === args.className
@@ -123,71 +358,93 @@ export const resolvers: Resolvers = {
         return character;
       });
     },
-    setCharacterBackground: (parent, args, context) => {
+    setCharacterBackground: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.backgroundName = args.name;
         return character;
       });
     },
-    setAbilityScores: (parent, args, context) => {
+    setAbilityScores: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
-        character.ability = { ...character.ability, ...args.abilities };
+        character.abilityScores = {
+          ...character.abilityScores,
+          ...args.abilityScores,
+        };
         return character;
       });
     },
-    setProficiencies: (parent, args, context) => {
+    setProficiencies: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.proficiencies = {
           armor: [
-            ...character.proficiencies?.armor,
-            ...args.proficiencies?.armor,
+            ...args.proficiencies.armor.map((proficiency) => ({
+              item: proficiency,
+              sourceType: CharacterDataSource.Other,
+            })),
           ],
           weapon: [
-            ...character.proficiencies?.weapon,
-            ...args.proficiencies?.weapon,
+            ...args.proficiencies.weapon.map((proficiency) => ({
+              item: proficiency,
+              sourceType: CharacterDataSource.Other,
+            })),
           ],
           language: [
-            ...character.proficiencies?.language,
-            ...args.proficiencies?.language,
+            ...args.proficiencies.language.map((proficiency) => ({
+              item: proficiency,
+              sourceType: CharacterDataSource.Other,
+            })),
           ],
-          tool: [...character.proficiencies?.tool, ...args.proficiencies?.tool],
+          tool: [
+            ...args.proficiencies.tool.map((proficiency) => ({
+              item: proficiency,
+              sourceType: CharacterDataSource.Other,
+            })),
+          ],
           savingThrow: [
-            ...character.proficiencies?.savingThrow,
-            ...args.proficiencies?.savingThrow,
+            ...args.proficiencies.savingThrow.map((proficiency) => ({
+              item: proficiency,
+              sourceType: CharacterDataSource.Other,
+            })),
+          ],
+          skill: [
+            ...args.proficiencies.skill.map((proficiency) => ({
+              item: proficiency,
+              sourceType: CharacterDataSource.Other,
+            })),
           ],
         };
         return character;
       });
     },
-    setHitPointMaximum: (parent, args, context) => {
+    setHitPointMaximum: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.hitPointMaximum = args.hitPoint;
         return character;
       });
     },
-    setHitPointCurrent: (parent, args, context) => {
+    setHitPointCurrent: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.hitPointCurrent = args.hitPoint;
         return character;
       });
     },
-    addItem: (parent, args, context) => {
+    addItem: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         return character;
       });
     },
-    removeItem: (parent, args, context) => {
+    removeItem: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         return character;
       });
     },
-    setCoins: (parent, args, context) => {
+    setCoins: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.coins = { ...character.coins, ...args.coins };
         return character;
       });
     },
-    addFeature: (parent, args, context) => {
+    addFeature: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.features = [
           ...character.features,
@@ -198,7 +455,7 @@ export const resolvers: Resolvers = {
         return character;
       });
     },
-    updateFeature: (parent, args, context) => {
+    updateFeature: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.features[args.index] = {
           entries: args.entries,
@@ -206,7 +463,7 @@ export const resolvers: Resolvers = {
         return character;
       });
     },
-    removeFeature: (parent, args, context) => {
+    removeFeature: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         character.features[args.index] = {
           entries: [],
@@ -214,10 +471,10 @@ export const resolvers: Resolvers = {
         return character;
       });
     },
-    levelUp: (parent, args, context) => {
+    levelUp: (_parent, args, context) => {
       return updateCharacter(context, args.characterName, (character) => {
         return character;
       });
     },
   },
-};
+} satisfies Resolvers;
